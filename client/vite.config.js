bd3b855257cc4e47cd00689af265333140551ed4
Paul <ttphat2427@clc.fitus.edu.vn>
2026-05-17 12:48:10 +0700

    chore: update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['boombust.studev.id.vn'],
+  },
   server: {
     port: 5174,
     strictPort: true,
